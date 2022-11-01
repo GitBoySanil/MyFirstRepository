bc1d950b614749648089e10652c135ba7ffb5ce0
GitBoySanil <s.d.shetgaonkar@gmail.com>
2022-11-01 23:33:12 +0530

    hello2 file added

---
+++ hello2.java
@@ -0,0 +1,16 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello wod i am sanil");
+System.out.println("I have added a new line of code by User1");
+System.out.println("I have added a new line of code by User1");
+System.out.println("Hi pooja how are you");
+System.out.println("Hi i love you sonu");
+
+
+
+	}
+
+}
